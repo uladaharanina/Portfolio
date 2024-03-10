fcc575114c002ceeee46a1da156d8909b17b5259
wadewilsones <uladaharanina@gmail.com>
2024-03-09 19:50:00 -0500

    feat: add projects

--- src/layouts/Projects.js
+++ src/layouts/Projects.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import '../styles/Projects.css'
+import '../styles/Projects.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 export const Projects = () => { 
     return( 
         <div className='ProjectMainContainer'>
-            <main className='ProjectSectionContainer'>     
-                <h1>Projects</h1>
-                <section className='ProjectContainer'>
-                    <div>
-                        <img src=''></img>
-                        <p>Horror Interactive</p>
-                        <p>2023 - present</p>
-                    </div>
-                    <div>
-                        <img src=''></img>
-                        <p>Stock Price Analyser</p>
-                        <p>2023</p>
-                    </div>
-                    <div>
-                        <img src=''></img>
-                        <p>Scheduling App</p>
-                        <p>2023</p>
-                    </div>
-                    <div>
-                        <img src='' alt="Mood Tracker"></img>
-                        <p>Mood Tracker</p>
-                        <p>2023</p>
-                    </div>
-                </section>
-            </main>
-        </div>
+            <section className='carousel_container'>
+            <Carousel>
+      <section className='carousel_items'>
+      <Carousel.Item>
 
+        <img
+          className="d-block w-100"
+          src="./images/order_online.png"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      </section>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/mood_tracker.png"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/horrors.png"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+            </section>
+        </div>
     
     )
 
--- src/layouts/Skills.js
+++ src/layouts/Skills.js
@@ -31,13 +31,15 @@ export const Skills = () => {
         <section id="storyline">
           <h2>The Storyline</h2>
           <div className='storyLine_paragraph_data' id="storyLine_paragraph_data_first_block">
-            <p>Started my Journey as a Graphic Designer in 2019. I worked at small publishing house and just got familiar with web design.</p>
+<p>I started my journey as a graphic designer in 2019. I worked at a small publishing house and got acquainted with web design.
+</p>
           </div>
           <div className='storyLine_paragraph_data'>
-            <p>After moving to the US I found myself in love with software development. In 2020 started to utilize tools such as Node.js, MongoDB, and React.js. This was the time when I realized I wanted to dive into the field and went to pursue further education. My focus moved to full-stack development. </p>
+            <p>
+After moving to the US, I found myself in love with software development. In 2020, we started to utilize tools such as Node.js, MongoDB, and React.js. Back then was the time when I realized I wanted to dive into the field and went to pursue further education. My focus moved to full-stack development.</p>
           </div>
           <div className='storyLine_paragraph_data'>
-            <p> I graduated with a Bachelor of Science in Computer Science in June 2023. I have multiple complex projects including creating my video game for mobile platforms. Currently, I work on a start-up as a software developer and additionally, I mentor students in languages such as Python, Java, and JavaScript. </p>
+            <p>I graduated with a Bachelor of Science in Computer Science in June 2023. I have multiple complex projects, including the creation of my video game for mobile platforms. Currently, I work on a start-up as a software developer, and additionally, I mentor students in languages such as Python, Java, and JavaScript.</p>
           </div>
         </section>
     </main>
